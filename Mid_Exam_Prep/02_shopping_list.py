77241d9dc379a19f0095f52c14e8d95d158e302b
DarinaStoyanova <darinastoyanovast@gmail.com>
2025-08-28 20:23:52 +0300

    Add missing exercises

---
+++ Mid_Exam_Prep/02_shopping_list.py
@@ -0,0 +1,37 @@
+initial_list_groceries = input().split("!")
+command = input()
+while command != "Go Shopping!":
+    item_and_new_command = command.split(" ")
+    new_command = item_and_new_command[0]
+    item = item_and_new_command[1]
+    if new_command == "Urgent":
+        if item in initial_list_groceries:
+            command = input()
+            continue
+        else:
+            initial_list_groceries.insert(0, item)
+    elif new_command == "Unnecessary":
+        if item in initial_list_groceries:
+            initial_list_groceries.remove(item)
+        else:
+            command = input()
+            continue
+    elif new_command == "Correct":
+        if item_and_new_command[1] in initial_list_groceries:
+            new_item = item_and_new_command[2]
+            initial_list_groceries = [new_item if item_and_new_command[1] in initial_list_groceries else item_and_new_command]
+            initial_list_groceries.remove(item_and_new_command[1])
+        else:
+            command = input()
+            continue
+    elif command == "Rearrange":
+        if item in initial_list_groceries:
+            initial_list_groceries.remove(item)
+            initial_list_groceries.append(item)
+        else:
+            command = input()
+            continue
+    command = input()
+
+initial_list_groceries = ', '.join(initial_list_groceries)
+print(initial_list_groceries)
