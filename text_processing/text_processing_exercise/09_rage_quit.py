18c3a65e73a56d6d17e6a6c9226b8e4f26987a84
DarinaStoyanova <darinastoyanovast@gmail.com>
2025-10-24 16:48:51 +0300

    more exercises

---
+++ text_processing/text_processing_exercise/09_rage_quit.py
@@ -0,0 +1,18 @@
+text = input()
+final_text = ""
+sub_string = ""
+repetitions = ""
+for index in range(len(text)):
+    if not text[index].isdigit():
+        sub_string += text[index].upper()
+    else:
+        for next_characters in range(index, len(text)):
+            if not text[next_characters].isdigit():
+                break
+            repetitions += text[next_characters]
+        final_text += sub_string * int(repetitions)
+        sub_string = ""
+        repetitions = ""
+
+print(f"Unique symbols used: {len(set(final_text))}")
+print(final_text)
