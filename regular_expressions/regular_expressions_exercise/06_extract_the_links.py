23f54841e5f9fd5a37405a820af864d1998df9bc
DarinaStoyanova <darinastoyanovast@gmail.com>
2025-11-13 11:27:37 +0200

    more exercises

--- regular_expressions/regular_expressions_exercise/06_extract_the_links.py
+++ regular_expressions/regular_expressions_exercise/06_extract_the_links.py
@@ -1,12 +1,10 @@
 import re
 text = input()
-while text != " ":
-    pattern = r"\s((www\.[a-zA-Z0-9]+[a-zA-Z0-9\-]*)(\.[a-z]+)+)"
-    matches = re.sea
+regex = r"((www\.[a-zA-Z0-9\-]+)(\.[a-z]+)+)"
 
+while text:
+    match = re.search(regex,text)
+    if match:
+        link = match.group(1)
+        print(link)
     text = input()
-    # pattern = r"\s((www\.[a-zA-Z0-9]+[a-zA-Z0-9\-]*)(\.[a-z]+)+)"
-    # matches = re.findall(pattern,text)
-    # for match in matches:
-    #     print(match[0])
-    # text = input()
